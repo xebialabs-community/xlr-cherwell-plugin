0fdc9065637d39eb8d5bbbf7cc8ba0241178f173
ladamato <ladamato@users.noreply.github.com>
2020-07-21 18:46:10 -0700

    updated license year, integrationTest and demo now use version 9.7

--- src/test/resources/docker/initialize/initialize_data.sh
+++ src/test/resources/docker/initialize/initialize_data.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-# Copyright 2019 XEBIALABS
+# Copyright 2020 XEBIALABS
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 #
@@ -13,7 +13,7 @@ SCRIPT=$(readlink -f "$0")
 # Absolute path this script is in, thus /home/user/bin
 SCRIPTPATH=$(dirname "$SCRIPT")
 
-####################### Endever server data
+####################### Cherwell server data
 
 
 wget --http-user=admin --http-password=admin --auth-no-challenge \
